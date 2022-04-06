57171ca6527dbe19ce5232536d8585771a727911
Lucheezou <lukevincent4@gmail.com>
2022-04-06 19:54:13 +0800

    exen

---
+++ edenscript.js
@@ -0,0 +1,104 @@
+
+window.addEventListener("scroll", function(){
+
+    var reveal1 = document.getElementById('reveal');
+    var windowHeight = window.innerHeight;
+    var elementTop = reveal1.getBoundingClientRect().top;
+    var elementVisible = 500;
+
+    if (elementTop < windowHeight - elementVisible) {
+      reveal1.classList.add("active");
+    } else {
+      reveal1.classList.remove("active");
+    }
+
+}
+);
+
+window.addEventListener("scroll", function(){
+
+    var reveal1 = document.getElementById('reveal1');
+    var windowHeight = window.innerHeight;
+    var elementTop = reveal1.getBoundingClientRect().top;
+    var elementVisible = 300;
+
+    if (elementTop < windowHeight - elementVisible) {
+      reveal1.classList.add("active");
+    } else {
+      reveal1.classList.remove("active");
+    }
+
+}
+);
+
+window.addEventListener("scroll", function(){
+
+    var reveal1 = document.getElementById('reveal2');
+    var windowHeight = window.innerHeight;
+    var elementTop = reveal1.getBoundingClientRect().top;
+    var elementVisible = 300;
+
+    if (elementTop < windowHeight - elementVisible) {
+      reveal1.classList.add("active");
+    } else {
+      reveal1.classList.remove("active");
+    }
+
+}
+);
+
+
+
+window.addEventListener("scroll", function(){
+
+    var reveal1 = document.getElementById('reveal3');
+    var windowHeight = window.innerHeight;
+    var elementTop = reveal1.getBoundingClientRect().top;
+    var elementVisible = 300;
+
+    if (elementTop < windowHeight - elementVisible) {
+      reveal1.classList.add("active");
+    } else {
+      reveal1.classList.remove("active");
+    }
+
+}
+);
+
+
+
+
+window.addEventListener("scroll", function(){
+
+    var reveal1 = document.getElementById('reveal4');
+    var windowHeight = window.innerHeight;
+    var elementTop = reveal1.getBoundingClientRect().top;
+    var elementVisible = 300;
+
+    if (elementTop < windowHeight - elementVisible) {
+      reveal1.classList.add("active");
+    } else {
+      reveal1.classList.remove("active");
+    }
+
+}
+);
+
+
+
+
+window.addEventListener("scroll", function(){
+
+    var reveal1 = document.getElementById('reveal5');
+    var windowHeight = window.innerHeight;
+    var elementTop = reveal1.getBoundingClientRect().top;
+    var elementVisible = 300;
+
+    if (elementTop < windowHeight - elementVisible) {
+      reveal1.classList.add("active");
+    } else {
+      reveal1.classList.remove("active");
+    }
+
+}
+);
